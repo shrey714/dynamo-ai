736381c7f75b28a98c43a96d0dfd371bb372f2be
shrey <pshrey0000@gmail.com>
2025-03-25 00:19:40 +0530

    small change

--- components/dashboard/theme-toggler.tsx
+++ components/dashboard/theme-toggler.tsx
@@ -5,25 +5,31 @@ import { useTheme } from "next-themes";
 import { MonitorIcon } from "lucide-react";
 
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { useEffect, useState } from "react";
 
 export const ModeToggle = () => {
   const { setTheme, theme } = useTheme();
+  const [shrey, setshrey] = useState<string | undefined>();
+  useEffect(() => {
+    setshrey(theme);
+  }, [theme]);
+
   return (
     <ToggleGroup
       type="single"
       onValueChange={(val) => {
         setTheme(val);
       }}
-      value={theme}
+      value={shrey}
       variant="outline"
     >
-      <ToggleGroupItem value="light" aria-label="Toggle light">
+      <ToggleGroupItem value="light" aria-label="light">
         <SunIcon className="h-4 w-4" />
       </ToggleGroupItem>
-      <ToggleGroupItem value="dark" aria-label="Toggle dark">
+      <ToggleGroupItem value="system" aria-label="system">
         <MonitorIcon className="h-4 w-4" />
       </ToggleGroupItem>
-      <ToggleGroupItem value="system" aria-label="Toggle system">
+      <ToggleGroupItem value="dark" aria-label="dark">
         <MoonIcon className="h-4 w-4" />
       </ToggleGroupItem>
     </ToggleGroup>
